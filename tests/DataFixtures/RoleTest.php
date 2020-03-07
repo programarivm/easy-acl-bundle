b22b60428ae46eb963b397bb7e39e6a9be94d781
programarivm <info@programarivm.com>
2020-03-07 18:25:26 +0000

    Programarivm\EasyAclBundle\Tests\DataFixtures\RoleTest has been added

---
+++ tests/DataFixtures/RoleTest.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace Programarivm\EasyAclBundle\Tests\DataFixtures;
+
+use Programarivm\EasyAclBundle\EasyAcl;
+use Programarivm\EasyAclBundle\Entity\Role;
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class RoleTest extends WebTestCase
+{
+    private static $easyAcl;
+
+    private static $em;
+
+    public static function setUpBeforeClass()
+    {
+        $kernel = static::createKernel();
+        $kernel->boot();
+
+        self::$container = $kernel->getContainer();
+
+        self::$easyAcl = self::$container->get('programarivm.easy_acl');
+        self::$em = self::$container->get('doctrine.orm.entity_manager');
+    }
+
+    /**
+     * @test
+     */
+    public function load()
+    {
+        foreach (self::$easyAcl->getRoles() as $item) {
+            $role = (new Role())
+                ->setType($item['name'])
+                ->setHierarchy($item['hierarchy']);
+
+            self::$em->persist($role);
+        }
+
+        self::$em->flush();
+
+        $this->assertTrue(true);
+    }
+}
