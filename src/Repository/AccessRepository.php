315ba033713b5eee2a55066b0fc664371868af64
programarivm <info@programarivm.com>
2020-03-11 19:41:56 +0000

    isAllowed method implemented in AccessRepository

--- src/Repository/AccessRepository.php
+++ src/Repository/AccessRepository.php
@@ -13,6 +13,22 @@ class AccessRepository extends ServiceEntityRepository
         parent::__construct($registry, Access::class);
     }
 
+    public function isAllowed(string $role, string $route): ?bool
+    {
+        $qb = $this->createQueryBuilder('a');
+
+        return (bool) $qb
+            ->where('a.role = :role')
+            ->andWhere('a.route = :route')
+            ->setParameters([
+                'role' => $role,
+                'route' => $route,
+            ])
+            ->getQuery()
+            ->getOneOrNullResult()
+        ;
+    }
+
     public function deleteAll()
     {
         $dql = 'DELETE FROM Programarivm\EasyAclBundle\Entity\Access';
